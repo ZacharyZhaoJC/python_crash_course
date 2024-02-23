927fc0043cf04c2826e02f08d525fc8031b5edd3
ZacharyZhaoJC <zhaojiachen.zachary@gmail.com>
2024-02-23 21:47:54 +0800

    chapter 2

---
+++ chapter_2_variables_and_simple_data_types/exercises/exercise_02_01_simple_message.py
@@ -0,0 +1,4 @@
+# 2-1. Simple Message: Assign a message to a variable, and then print that message.
+
+message = "You really do understand the definition of uniqueness."
+print(message)
---
+++ chapter_2_variables_and_simple_data_types/exercises/exercise_02_02_simple_message.py
@@ -0,0 +1,8 @@
+# 2-2. Simple Messages: Assign a message to a variable, and print that message.
+# Then change the value of the variable to a new message, and print the new message.
+
+message = "You really do understand the definition of uniqueness, "
+print(message)
+
+message = "which is not as simple as breathing."
+print(message)
