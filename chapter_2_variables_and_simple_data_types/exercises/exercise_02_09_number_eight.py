bed92b7bce0472187cd395764ffdf26fda43d411
ZacharyZhaoJC <zhaojiachen.zachary@gmail.com>
2024-02-23 23:39:05 +0800

    number exercise

---
+++ chapter_2_variables_and_simple_data_types/exercises/exercise_02_09_number_eight.py
@@ -0,0 +1,7 @@
+# 2-9. Number Eight: Write addition, subtraction, multiplication, and division operations that
+# each result in the number 8. Be sure to enclose your operations in print() calls to see the results.
+
+print(5+3)
+print(9-1)
+print(2*4)
+print(int(16/2))
---
+++ chapter_2_variables_and_simple_data_types/exercises/exercise_02_10_favorite_number.py
@@ -0,0 +1,8 @@
+# 2-10. Favorite Number: Use a variable to represent your favorite number. Then, using that
+# variable, create a message that reveals your favorite number. Print that message.
+
+favorite_number = 1209
+message = (f'My favorite number is {favorite_number}, but there is a saying goes that "Some people cant be forgotten, '
+           f'but dont have to be together."')
+
+print(message)
