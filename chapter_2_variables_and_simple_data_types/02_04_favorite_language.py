a7e9b04b972adb096848885f42249f87de2a6e54
ZacharyZhaoJC <zhaojiachen.zachary@gmail.com>
2024-02-23 21:56:17 +0800

    lstrip(), rstrip() and strip()

---
+++ chapter_2_variables_and_simple_data_types/02_04_favorite_language.py
@@ -0,0 +1,15 @@
+# To ensure that no whitespace exists at the right side of a string, use the rstrip() method:
+favorite_language = "python "
+print(favorite_language)
+print(favorite_language.rstrip())
+
+favorite_language = favorite_language.rstrip()
+print(favorite_language)
+
+# You can also strip whitespace from the left side of a string using the lstrip() method,
+# or from both sides at once using strip()
+favorite_language = "  python "
+print(favorite_language.lstrip())
+print(favorite_language.rstrip())
+print(favorite_language.strip())
+print(favorite_language)
